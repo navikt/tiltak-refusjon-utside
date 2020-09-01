1f3e56d22d5651012b696bd3c6d34964519b6d49
Mats Bolstad <mbolstad@gmail.com>
2020-09-01 16:15:15 +0200

    Ekstern dekoratør

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import reverseProxy from "./reverse-proxy";
+import axios from 'axios';
 
 const router = express.Router();
 
@@ -32,13 +33,18 @@ const setup = () => {
     // serve static files
     router.use(express.static(path.join(__dirname, "../build"), { index: false }));
 
-    router.use('*', (req, res) => {
+    router.use('*', async (req, res) => {
+        const response = await axios.get(process.env.DECORATOR_URL);
+        const body = response.data;
+
+        const { document } = new JSDOM(body).window;
         res.render('index.html', {
-            NAV_SCRIPTS: `<script src="${process.env.DECORATOR_SCRIPT}"></script>`,
-            NAV_STYLES: `<link rel="stylesheet" href="${process.env.DECORATOR_STYLING}"/>`,
-            NAV_HEADING: "<div id='header'></div><script>NAVSPA.internarbeidsflatefs(document.getElementById('header'), { appname: 'Tiltaksgjennomføring - refusjon', toggles: { visEnhet: false, visEnhetVelger: false, visSokefelt: false, visVeilder: false }})</script>",
-            NAV_SKIPLINKS: '',
-            NAV_FOOTER: '',
+            NAV_SCRIPTS: document.getElementById('scripts').innerHTML,
+            NAV_STYLES: document.getElementById('styles').innerHTML,
+            NAV_SKIPLINKS: document.getElementById('skiplinks').innerHTML,
+            NAV_HEADING: document.getElementById('header-withmenu').innerHTML,
+            NAV_FOOTER: document.getElementById('footer-withmenu').innerHTML,
+            NAV_MENU_RESOURCES: document.getElementById('megamenu-resources').innerHTML,
         });
     });
 
