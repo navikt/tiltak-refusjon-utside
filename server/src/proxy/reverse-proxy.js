d528f4638146553939ebd059477b0cbaa8670e03
Mats Bolstad <mbolstad@gmail.com>
2020-10-28 10:03:08 +0100

    Tester logging med winston

--- server/src/proxy/reverse-proxy.js
+++ server/src/proxy/reverse-proxy.js
@@ -2,6 +2,16 @@ import proxy from 'express-http-proxy';
 import authUtils from '../auth/utils';
 import config from '../config';
 
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            format: format.combine(format.timestamp(), loggerFormat),
+            filename: path.join(__dirname, "../", "logs", "combined.log"),
+        }),
+    ],
+});
+
 const setup = (router, tokenxClient) => {
     router.use("/api", proxy(config.api.url, {
         parseReqBody: false,
@@ -9,6 +19,7 @@ const setup = (router, tokenxClient) => {
             return req.originalUrl;
         },
         proxyReqOptDecorator: (options, req) => {
+            logger.info("proxy");
             return new Promise(((resolve, reject) =>
                     authUtils.getOnBehalfOfAccessToken(
                         req.session.tokens.id_token, tokenxClient
