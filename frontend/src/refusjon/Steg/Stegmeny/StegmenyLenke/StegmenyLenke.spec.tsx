56b1a33360189d9b69be18c0c7f5d17b76086ca3
Mats Bolstad <mbolstad@gmail.com>
2020-11-02 22:28:41 +0100

    Sletter kode som komfra copy/paste

--- frontend/src/refusjon/Steg/Stegmeny/StegmenyLenke/StegmenyLenke.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import StegmenyLenke from './StegmenyLenke';
-
-test('Test that <StegmenyLenke> renders correctly', () => {
-    const wrapper = shallow(<StegmenyLenke />);
-    expect(wrapper).toHaveLength(1);
-});
