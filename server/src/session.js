7f042c1d0df302d9f9e9fffa7e57d905af58db58
Mats Bolstad <mbolstad@gmail.com>
2020-11-09 13:37:23 +0100

    Oppsett med redis

--- server/src/session.js
+++ server/src/session.js
@@ -1,56 +1,45 @@
-import session from 'express-session';
 import config from './config';
+import redis from 'redis';
+import session from 'express-session';
 
 const SESSION_MAX_AGE_MILLISECONDS = 60 * 60 * 1000;
 
 const setup = (app) => {
     app.set('trust proxy', 1);
-    // if (process.env.NODE_ENV === 'development') {
-    app.use(
-        session({
-            cookie: {
-                maxAge: SESSION_MAX_AGE_MILLISECONDS,
-                sameSite: 'lax',
-                httpOnly: true,
-            },
-            secret: config.server.sessionKey,
-            name: config.server.sessionCookieName,
-            resave: false,
-            saveUninitialized: true,
-        })
-    );
-    // } else {
-    //     const RedisStore = require('connect-redis')(session);
-    //
-    //     const client = redis.createClient({
-    //         host: config.redis.host,
-    //         password: config.redis.password,
-    //         port: config.redis.port,
-    //     });
-    //     client.unref();
-    //     client.on('error', console.log);
-    //
-    //     const store = new RedisStore({
-    //         client: client,
-    //         disableTouch: true,
-    //     });
-    //
-    //     app.use(
-    //         session({
-    //             cookie: {
-    //                 maxAge: SESSION_MAX_AGE_MILLISECONDS,
-    //                 secure: true,
-    //                 httpOnly: true,
-    //                 sameSite: 'lax'
-    //             },
-    //             name: config.server.cookieName,
-    //             saveUninitialized: true,
-    //             secret: config.server.sessionKey,
-    //             store: store,
-    //             resave: false,
-    //         })
-    //     );
-    // }
+    const options = {
+        cookie: {
+            maxAge: SESSION_MAX_AGE_MILLISECONDS,
+            sameSite: 'lax',
+            httpOnly: true,
+        },
+        secret: config.server.sessionKey,
+        name: config.server.cookieName,
+        resave: false,
+        saveUninitialized: false,
+        unset: 'destroy',
+    };
+    if (process.env.NODE_ENV !== 'development') {
+        options.cookie.secure = true;
+        options.store = setupRedis();
+    }
+    app.use(session(options));
+};
+
+const setupRedis = () => {
+    const RedisStore = require('connect-redis')(session);
+
+    const client = redis.createClient({
+        host: config.redis.host,
+        password: config.redis.password,
+        port: config.redis.port,
+    });
+    client.unref();
+    client.on('error', console.log);
+
+    return new RedisStore({
+        client: client,
+        disableTouch: true,
+    });
 };
 
 export default { setup };
