5d53ee8753f0f75baf321b07c1fb46939b2e070f
Mats Bolstad <mbolstad@gmail.com>
2020-11-16 14:47:43 +0100

    Bruker www.nav.no som href i index.html. Lokalt sjekkes auth mot innlogget bruker.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const axios = require('axios');
 
 module.exports = function (app) {
+    app.use('/api', createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true }));
+
     app.use('/dekoratoren/env', async (req, res) => {
         const response = await axios.get('https://www.nav.no/dekoratoren/env?context=arbeidsgiver');
         res.json({ ...response.data, APP_URL: '/dekoratoren', LOGOUT_URL: '/logout' });
     });
-    app.use('/dekoratoren/api/auth', (req, res) => {
-        res.json({ authenticated: true, name: '' });
+    app.use('/dekoratoren/api/auth', async (req, res) => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/innlogget-bruker', {
+                headers: req.headers,
+            });
+            res.json({ authenticated: true, name: response.data.identifikator });
+        } catch (error) {
+            res.json({ authenticated: false });
+        }
     });
     app.use('/logout', (req, res) => {
         res.clearCookie('tokenx-token');
