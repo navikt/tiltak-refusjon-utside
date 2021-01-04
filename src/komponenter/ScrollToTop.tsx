f0bedabaf5b7fa8e4beb5c2cf0abf0e75476f313
Sindre Dahl Løken <sindre.loken@nav.no>
2021-01-04 16:54:06 +0100

    scroll-to-top komponent Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

---
+++ src/komponenter/ScrollToTop.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop: FunctionComponent = () => {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+    return null;
+};
+
+export default ScrollToTop;
