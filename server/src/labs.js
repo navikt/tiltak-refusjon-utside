0a586d492b8270ff7cb7a92e0f25eb036640c27c
Mats Bolstad <mbolstad@gmail.com>
2021-01-27 12:49:18 +0100

    Legger til manglende import

--- server/src/labs.js
+++ server/src/labs.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from './cors';
 import path from 'path';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import axios from 'axios';
 
 const server = express();
 
