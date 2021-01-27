6cae781dd17d16a72ea9baf55e64fa5f17e87a0f
Mats Bolstad <mbolstad@gmail.com>
2021-01-27 13:42:25 +0100

    Hente HOST uten config.js, for å unngå full sjekk av miljøvariable

--- server/src/cors.js
+++ server/src/cors.js
@@ -1,7 +1,5 @@
-import config from './config';
-
 export default (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', config.server.host);
+    res.setHeader('Access-Control-Allow-Origin', process.env.HOST);
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, X-Requested-With');
     return next();
