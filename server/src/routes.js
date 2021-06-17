5213766da4f948ebe1299893bb4fa382ece6066a
Mats Bolstad <mbolstad@gmail.com>
2021-06-17 17:18:02 +0200

    SPA routing

--- server/src/routes.js
+++ server/src/routes.js
@@ -70,8 +70,11 @@ const setup = (tokenxClient, idportenClient) => {
     apiProxy.setup(router, tokenxClient);
     decoratorProxy.setup(router);
 
-    router.use(express.static(path.join(__dirname, '../build', 'index.html')));
+    router.use(express.static(path.join(__dirname, '../build')));
 
+    router.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    });
     return router;
 };
 
